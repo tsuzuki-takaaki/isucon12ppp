775eeca7f07ea0e4aff5dd88073bc10d848484e4
isucon-tsuzuki-takaaki <echo.tsuzuki.takaaki@gmail.com>
2024-09-15 12:05:17 +0000

    Add deploy script

--- memo_script.sh
+++ memo_script.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# serviceの停止(前から止めていく)
+sudo systemctl stop nginx
+sudo systemctl stop isuports.service
+sudo systemctl stop mysql
+
+# TODO: Please refactor
+ssh isu2 sudo systemctl stop nginx
+ssh isu2 sudo systemctl stop isuports.service
+ssh isu2 sudo systemctl stop mysql
+
+ssh isu3 sudo systemctl stop nginx
+ssh isu3 sudo systemctl stop isuports.service
+ssh isu3 sudo systemctl stop mysql
 # /etcをuserレベルで管理している/home/isuconetc/を、ほかserverにscpする
 # isu2,isu3の設定は/etc/hostsをいじるかsshconfig書くか
 # nginxとかのserverによって振り分ける系はこれやめた方がいいかも
@@ -12,3 +25,16 @@ scp -r etc/ isu3:~/
 # See man scp
 scp -r webapp/ isu2:~/
 scp -r webapp/ isu3:~/
+
+# serviceの再起動(systemd unit fileを触った場合は、sudo systemctl daemon reload)
+sudo systemctl start nginx
+sudo systemctl start isuports.service
+sudo systemctl start mysql
+
+ssh isu2 sudo systemctl start nginx
+ssh isu2 sudo systemctl start isuports.service
+ssh isu2 sudo systemctl start mysql
+
+ssh isu3 sudo systemctl start nginx
+ssh isu3 sudo systemctl start isuports.service
+ssh isu3 sudo systemctl start mysql
