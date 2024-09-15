db7127ef0cfefc3ef1117c807c81be752a6692c6
isucon-tsuzuki-takaaki <echo.tsuzuki.takaaki@gmail.com>
2024-09-15 11:52:50 +0000

    Add memoscript for sync

--- memo_script.sh
+++ memo_script.sh
@@ -4,6 +4,11 @@
 # isu2,isu3の設定は/etc/hostsをいじるかsshconfig書くか
 # nginxとかのserverによって振り分ける系はこれやめた方がいいかも
 # systemd, mysqld
-scp -r etc/ isu2:etc/
-scp -r etc/ isu3:etc/
+scp -r etc/ isu2:~/
+scp -r etc/ isu3:~/
 
+# Applicationの設定も同様にcopy
+# sizeでかいのと、path指定本当にこれで正解？
+# See man scp
+scp -r webapp/ isu2:~/
+scp -r webapp/ isu3:~/
